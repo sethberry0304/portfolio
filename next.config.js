c86f20179299fc4ab39b9fb9bf33c8faead26a5d
张钦志 <seththemoonman@mac.mynetworksettings.com>
2025-10-02 03:05:30 -0400

    build: ignore eslint during builds

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   images: {
-    domains: ['i0.hdslb.com', 'i1.hdslb.com', 'i2.hdslb.com', 'images.unsplash.com'],
-    remotePatterns: [
-      { protocol: 'https', hostname: 'i0.hdslb.com' },
-      { protocol: 'https', hostname: 'i1.hdslb.com' },
-      { protocol: 'https', hostname: 'i2.hdslb.com' },
-    ],
+    domains: ['i0.hdslb.com','i1.hdslb.com','i2.hdslb.com','images.unsplash.com'],
   },
 };
-
 module.exports = nextConfig;
